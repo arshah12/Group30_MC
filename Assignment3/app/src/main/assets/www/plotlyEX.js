823f7a13b2452ed66e6b3f2863b94d0a16ced8b4
ayancharz <ayancharz@gmail.com>
2018-04-20 00:49:34 -0700

    plotly finally done

--- Assignment3/app/src/main/assets/www/plotlyEX.js
+++ Assignment3/app/src/main/assets/www/plotlyEX.js
@@ -1,35 +1,38 @@
 
-TESTER = document.getElementById('tester');
-
-
-/* Current Plotly.js version */
-console.log( Plotly.BUILD );
-
 function plotgraph(msg){
 
-       var x = msg[0][0];
-       var y = msg[0][1];
-       var z= msg[0][2];
-    document.getElementById('para').innerHTML = msg;
-    var j=0;
-    console.log(msg[0][0][0]);
+//       var x = msg[0][0];
+//       var y = msg[0][1];
+//       var z= msg[0][2];
+//    var j=0;
+//    console.log(msg[0][0][0]);
+
+    for (var dataItems = 0; dataItems<msg.length; dataItems++){
+
+        var x = msg[dataItems][0];
+        var y = msg[dataItems][1];
+        var z = msg[dataItems][2];
 
 
+        Plotly.plot('graph', [{
+              type: 'scatter3d',
+              mode: 'lines',
+              x: x,
+              y: y,
+              z: z,
+              opacity: 1,
+              line: {
+                width: 6,
+                color: 0,
+                reversescale: false
+              }
+            }], {
+              height: 400,
+              width: 368
+            });
 
-    Plotly.plot(TESTER, [{
-      type: 'scatter3d',
-      mode: 'lines',
-      x: x,
-      y: y,
-      z: z,
-      opacity: 1,
-      line: {
-        width: 6,
-        color: 0,
-        reversescale: false
-      }
-    }], {
-      height: 640
-    });
+
+    }
+
 
 }
